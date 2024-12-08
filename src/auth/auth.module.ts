1cc4670ceecfe54afb03a53db619d481b04b70cf
João Lucas Rodrigues <joaosp7@gmail.com>
2024-12-08 15:36:12 -0300

    finished auth jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstant } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule , JwtModule.register({
+    secret: jwtConstant.secret,
+    signOptions: {expiresIn: '60s'},
+  })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 
 
 export class AuthService{
-  constructor( private usersService: UsersService) {}
+  constructor( private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string) {
     const user = await this.usersService.findOneByUsername(username);
@@ -15,5 +18,10 @@ export class AuthService{
       return result;
     }
     return null;
+  }
+
+  async login(user:any) {
+    const payload = {username: user.username, user: user.Id}
+    return { access_token : this.jwtService.sign(payload),};
   }
 }
